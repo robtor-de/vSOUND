e9b28e90520af5bba0553fe95b6236d6ba1410f0
Robin Dietzel <robtor@PC-Robin.fritz.box>
2017-10-07 14:31:14 +0200

    made search function responsive

--- web/vSOUND/trctl/forms.py
+++ web/vSOUND/trctl/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class searchForm(forms.Form):
-    search_text = forms.CharField(label='Suchen', max_length=30)
+    search_text = forms.CharField(label='Suchen', max_length=30, widget=forms.TextInput(attrs={'class': 'form-control mr-sm-2', 'type': 'text', 'placeholder': 'Suchen'}))
