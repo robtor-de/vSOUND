e89c5a4237b0573ad3b66f1f4f71e572ab6f6680
Robin Dietzel <robtor@PC-Robin.fritz.box>
2017-10-09 21:25:37 +0200

    added basic models and made migrations

--- web/vSOUND/vote/models.py
+++ web/vSOUND/vote/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class vote_option(models.Model):
+    song = models.ForeignKey("votable_song")
+    v_count = models.IntegerField(default=0)
+
+
+class votable_song(models.Model):
+    file_name = models.TextField()
+    song_title = models.TextField()
+    song_artist = models.TextField()
+    song_album = models.TextField()
