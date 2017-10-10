4c995233cc37a16c665548849c0d0372b2f8470c
Robin Dietzel <robtor@PC-Robin.fritz.box>
2017-10-10 20:31:07 +0200

    added suspend scripts

--- web/vSOUND/vote/admin.py
+++ web/vSOUND/vote/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from vote.models import votable_song, suspended_song, vote_option, vote
 
 # Register your models here.
+admin.site.register(votable_song)
+admin.site.register(suspended_song)
+admin.site.register(vote_option)
+admin.site.register(vote)
