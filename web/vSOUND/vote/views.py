48c7ebfe038a0d1d3ddbf28d4f08e8823d370c42
Robin Dietzel <robtor@PC-Robin.fritz.box>
2017-10-11 23:14:27 +0200

    applying bootstrap theme

--- web/vSOUND/vote/views.py
+++ web/vSOUND/vote/views.py
@@ -1,14 +1,44 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from mpd import MPDClient
+from django.conf import settings
 from vote.models import vote_option as o
 
 # Create your views here.
 
+
+cli = MPDClient()
+
+def connect_mpd():
+    global cli
+
+    try:
+        cli.status()
+    except:
+        try:
+            MPDClient.connect(cli, settings.MPD_ADDRESS, settings.MPD_PORT)
+        except:
+            #TODO: Add here better support
+            print("MPD-Error")
+
+
 def vote_view(request):
+    global cli
+    connect_mpd()
+
+    playlist = cli.playlistinfo()
+    status = cli.status()
+
+    try:
+        current_song = playlist[int(status['song'])]["title"]
+        current_artist = playlist[int(status['song'])]["artist"]
+    except:
+        current_song = ""
+        current_artist = ""
+
     if(o.is_active()):
         options = o.objects.all()
-        return render(request, "vote/vote.html", {"options": options})
+        return render(request, "vote/vote.html", {"playlist": playlist, "status": status, "options": options, "song": current_song, "artist": current_artist})
     else:
         return render(request, "vote/vote_inactive.html")
 
